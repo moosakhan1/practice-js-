59c45fa7fa6b0bfae49d880e8df3b7c51f11cd12
moosakhan1 <144359037+moosakhan1@users.noreply.github.com>
2023-11-08 22:05:45 +0500

    practice set

---
+++ app.js
@@ -0,0 +1,28 @@
+
+let a = "har\"";
+console.log(a.length)
+
+let b = "this is a physics ";
+let c = b.includes("is a")
+console.log(c)
+console.log(b.includes("this"));
+
+let d1 = "moosa khan";
+let test1 = d1.startsWith("khan") 
+console.log(test1)
+
+let d = "moosa khan";
+let test = d.endsWith("khan") 
+console.log(test)
+
+let f = "MOOSA";
+let f1 = f.toLowerCase();
+console.log(f1)
+
+let g = "Pleas give Rs 1000";
+let g1 = g.slice(5,10);
+console.log(g1); 
+
+let pkwep = "mokjdc"
+pkwep[4] = "e"
+console.log(pkwep)
